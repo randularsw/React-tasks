086abd8c423662cce4b9e3f83c38b7a20f93baf2
Randula Wickramasuriya <randularsw@gmail.com>
2020-05-02 17:58:30 +0530

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import './App.css';
-import Movies from './components/movies';
+import React from "react";
+import "./App.css";
+import Movies from "./components/movies";
+import { Route, Redirect, Switch } from "react-router-dom";
+import NotFound from "./components/notFound";
 // import Counters from './components/counters';
 
 function App() {
   return (
-    <div className="App">
-      <Movies />
-    </div>
+    <main className="container">
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect exact from="/" to="/movies"></Redirect>
+        <Redirect to="not-found" />
+      </Switch>
+    </main>
   );
 }
-
 export default App;
