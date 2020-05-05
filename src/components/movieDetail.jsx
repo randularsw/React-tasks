d5ccdc9ea9d8dec7eaa7e1b936789ee92d21f85f
Randula Wickramasuriya <randularsw@gmail.com>
2020-05-05 15:03:47 +0530

    Add or edit movie details

--- src/components/movieDetail.jsx
+++ src/components/movieDetail.jsx
@@ -1,17 +1,184 @@
-import React from "react";
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import { Link } from "react-router-dom";
+import { getMovie, saveMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
 
-const MovieDetail = ({ match, history }) => {
-  return (
-    <div>
-      <h1>Movie ID: {match.params.id}</h1>
-      <button
-        className="btn btn-secondary"
-        onClick={() => history.push("/movies")}
-      >
-        Back to Movies
-      </button>
-    </div>
-  );
-};
+class MovieDetail extends Component {
+  state = {
+    movieModel: {
+      title: "",
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: "",
+    },
+    genres: [],
+    movieErrors: {},
+  };
+
+  schema = {
+    _id: Joi.string(),
+    title: Joi.string().required().label("Title"),
+    genreId: Joi.string().required().label("Genre"),
+    numberInStock: Joi.number()
+      .required()
+      .min(0)
+      .max(100)
+      .label("Number in Stock"),
+    dailyRentalRate: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Daily Rental Rate"),
+  };
+
+  componentDidMount() {
+    const genres = getGenres();
+    this.setState({ genres });
+    const movieId = this.props.match.params.id;
+    if (movieId === "new") return;
+
+    const movie = getMovie(movieId);
+    if (!movie) return this.props.history.replace("/not-found");
+    this.setState({ movieModel: this.mapToViewModel(movie) });
+  }
+
+  mapToViewModel(movie) {
+    return {
+      _id: movie._id,
+      title: movie.title,
+      genreId: movie.genre._id,
+      numberInStock: movie.numberInStock,
+      dailyRentalRate: movie.dailyRentalRate,
+    };
+  }
+
+  validate() {
+    const { error } = Joi.validate(this.state.movieModel, this.schema, {
+      abortEarly: false,
+    });
+    if (!error) return null;
+    const errors = {};
+    for (let item of error.details) {
+      this.state.movieErrors[item.path[0]] = item.message;
+    }
+    return errors;
+  }
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.movieErrors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const movieModel = { ...this.state.movieModel };
+    movieModel[input.name] = input.value;
+
+    this.setState({ movieModel, movieErrors: errors });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate();
+    this.setState({ movieErrors: this.state.movieErrors || {} });
+    if (errors) return;
+    console.log(this.state.movieModel);
+    saveMovie(this.state.movieModel);
+    this.props.history.push("/movies");
+  };
+  render() {
+    const { movieModel, movieErrors, genres } = this.state;
+    return (
+      <div className="my-5 mx-auto" style={{ width: "50%" }}>
+        <h4>movie</h4>
+        <Link to="/movies">Back to Movies</Link>
+        <br />
+        <br />
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              className="form-control"
+              value={movieModel.title}
+              onChange={this.handleChange}
+              name="title"
+            />
+            {movieErrors.title && (
+              <div className="alert alert-danger">{movieErrors.title}</div>
+            )}
+          </div>
+          <div className="formGroup">
+            <label htmlFor="genreId">Genre</label>
+            <select
+              name="genreId"
+              id="genreId"
+              className="form-control"
+              onChange={this.handleChange}
+              value={movieModel.genreId}
+            >
+              <option value=""></option>
+              {genres.map((genre) => (
+                <option
+                  key={genre._id}
+                  value={genre._id}
+                  // selected={genre._id === movieModel.genreId}
+                >
+                  {genre.name}
+                </option>
+              ))}
+            </select>
+            {movieErrors.genreId && (
+              <div className="alert alert-danger">{movieErrors.genreId}</div>
+            )}
+          </div>
+          <div className="form-group">
+            <label htmlFor="numberInStock">Number in Stock</label>
+            <input
+              type="number"
+              id="numberInStock"
+              className="form-control"
+              value={movieModel.numberInStock}
+              onChange={this.handleChange}
+              name="numberInStock"
+            />
+            {movieErrors.numberInStock && (
+              <div className="alert alert-danger">
+                {movieErrors.numberInStock}
+              </div>
+            )}
+          </div>
+          <div className="form-group">
+            <label htmlFor="dailyRentalRate">Daily Rental Rate</label>
+            <input
+              type="number"
+              id="dailyRentalRate"
+              className="form-control"
+              value={movieModel.dailyRentalRate}
+              onChange={this.handleChange}
+              name="dailyRentalRate"
+            />
+            {movieErrors.dailyRentalRate && (
+              <div className="alert alert-danger">
+                {movieErrors.dailyRentalRate}
+              </div>
+            )}
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default MovieDetail;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,79 +1,109 @@
-import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService';
-import Pagination from './pagination';
-import ListGroup from './listGroup';
-import { getGenres } from '../services/fakeGenreService';
-import MoviesTable from './moviesTable';
-import _ from 'lodash';
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Pagination from "./pagination";
+import ListGroup from "./listGroup";
+import { getGenres } from "../services/fakeGenreService";
+import MoviesTable from "./moviesTable";
+import _ from "lodash";
+import { Link } from "react-router-dom";
 
 export default class Movies extends Component {
-    state = {
-        movies: [],
-        genres: [],
-        pageSize: 4,
-        currentPage: 1,
-        sortColumn: { path: 'title', order: 'asc' }
-    }
+  state = {
+    movies: [],
+    genres: [],
+    pageSize: 4,
+    currentPage: 1,
+    sortColumn: { path: "title", order: "asc" },
+  };
 
-    componentDidMount() {
-        const genres = [{ name: 'All', _id: 1 }, ...getGenres()];
-        this.setState({ movies: getMovies(), genres });
-    }
+  componentDidMount() {
+    const genres = [{ name: "All", _id: 1 }, ...getGenres()];
+    this.setState({ movies: getMovies(), genres });
+  }
 
-    handleLike = (movie) => {
-        const movies = [...this.state.movies];
-        const index = movies.indexOf(movie);
-        movies[index].liked = !movies[index].liked;
-        this.setState({ movies });
-    }
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
 
-    handleDelete = (movie) => {
-        const movies = this.state.movies.filter(m => m._id !== movie._id);
-        this.setState({ movies: movies });
-    }
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
 
-    handleSort = (sortColumn) => {
-        this.setState({ sortColumn });
-    }
+  handleSort = (sortColumn) => {
+    this.setState({ sortColumn });
+  };
 
-    handlePageChange = (page) => {
-        this.setState({ currentPage: page });
-    }
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
 
-    handleGenreSelect = (genre) => {
-        this.setState({ selectedGenre: genre, currentPage: 1 });
-    }
+  handleGenreSelect = (genre) => {
+    this.setState({ selectedGenre: genre, currentPage: 1 });
+  };
 
-    paginate(items, pageNumber, pageSize) {
-        const startIndex = (pageNumber - 1) * pageSize;
-        return items.slice(startIndex, startIndex + pageSize);
-    }
+  paginate(items, pageNumber, pageSize) {
+    const startIndex = (pageNumber - 1) * pageSize;
+    return items.slice(startIndex, startIndex + pageSize);
+  }
 
-    render() {
-        const { length: count } = this.state.movies;
-        const { movies: allMovies, genres, pageSize, currentPage, selectedGenre, sortColumn } = this.state;
-        if (count === 0) {
-            return <h4>No movies yet.</h4>
-        }
-        // filter
-        const filtered = selectedGenre && selectedGenre._id !== 1 ? allMovies.filter(m => m.genre._id === selectedGenre._id) : allMovies;
-        // sort
-        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
-        // paginate
-        const movies = this.paginate(sorted, currentPage, pageSize);
-        return (
-            <div className="row container-fluid">
-                <div className="col-3" style={{ marginTop: '175px', marginLeft: '100px' }}>
-                    <ListGroup items={genres} selectedGenre={selectedGenre} onGenreSelect={this.handleGenreSelect} />
-                </div>
-                <div className="col m-5">
-                    <h4>{filtered.length} movies available</h4>
-                    <MoviesTable movies={movies} sortColumn={sortColumn} onLike={this.handleLike} onDelete={this.handleDelete} onSort={this.handleSort} />
-                    <div className='d-flex justify-content-center'>
-                        <Pagination count={filtered.length} pageSize={pageSize} currentPage={currentPage} onPageChange={this.handlePageChange} />
-                    </div>
-                </div>
-            </div>
-        );
+  render() {
+    const { length: count } = this.state.movies;
+    const {
+      movies: allMovies,
+      genres,
+      pageSize,
+      currentPage,
+      selectedGenre,
+      sortColumn,
+    } = this.state;
+    if (count === 0) {
+      return <h4>No movies yet.</h4>;
     }
+    // filter
+    const filtered =
+      selectedGenre && selectedGenre._id !== 1
+        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+        : allMovies;
+    // sort
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+    // paginate
+    const movies = this.paginate(sorted, currentPage, pageSize);
+    return (
+      <div className="row container-fluid">
+        <div
+          className="col-3"
+          style={{ marginTop: "175px", marginLeft: "100px" }}
+        >
+          <ListGroup
+            items={genres}
+            selectedGenre={selectedGenre}
+            onGenreSelect={this.handleGenreSelect}
+          />
+        </div>
+        <div className="col m-5">
+          <h4>{filtered.length} movies available</h4>
+          <Link to={"/movies/new"}> Add Movie</Link>
+          <MoviesTable
+            movies={movies}
+            sortColumn={sortColumn}
+            onLike={this.handleLike}
+            onDelete={this.handleDelete}
+            onSort={this.handleSort}
+          />
+          <div className="d-flex justify-content-center">
+            <Pagination
+              count={filtered.length}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageChange={this.handlePageChange}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
